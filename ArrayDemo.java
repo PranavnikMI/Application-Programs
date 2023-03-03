4dd48ef67001526b3eb82fca13a51f8bcd89a513
PranavnikMI <126877292+PranavnikMI@users.noreply.github.com>
2023-03-03 18:26:59 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,22 @@
+
+class ArrayDemo
+{
+    public static void main(String arg[])
+    {
+        //ways of creating single dimentional array
+
+        int Arr1[] = {10,20,30,40};
+        //  int Arr2[4] = {10,20,30,40};     Explicit size is not allowed in java
+
+        int Arr3[] = new int[4];          // dynamic  memory allocation
+        Arr3[0] = 10;
+        Arr3[1] = 20;
+        Arr3[2] = 30;
+        Arr3[3] = 40;
+
+
+        int Arr4[] = new int[]{10,20,30,40};      //Daynamic memory with initialisation list   
+
+    }
+}
+
