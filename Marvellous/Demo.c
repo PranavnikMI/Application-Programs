a2b92b1cd95bcdbcf700e1760cb12edd4696d688
PranavnikMI <126877292+PranavnikMI@users.noreply.github.com>
2024-03-11 20:33:43 +0530

    Add files via upload

---
+++ Marvellous/Demo.c
@@ -0,0 +1,14 @@
+// Folder both file incomplete complete using audio recording
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Jay Ganesh...\n");
+
+    if(argc == 2)
+    {
+        printf("welcome %s\n",argv[1]);
+    }
+    
+    return 0;
+}
