f48e36e67e390d44da41fe559d02344d50e29f04
PranavnikMI <126877292+PranavnikMI@users.noreply.github.com>
2024-03-11 20:18:35 +0530

    Add files via upload

---
+++ srcngfor/app/ng-for-demo/ng-for-demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for-demo',
+  templateUrl: './ng-for-demo.component.html',
+  styleUrls: ['./ng-for-demo.component.css']
+})
+export class NgForDemoComponent 
+{
+  public Batches = ["PPA","LB","WEB","PROJECT"];
+
+}
